135c488929b1d285b1ff4a37138d2b5273c39ea6
Anna Lotko <annaklotko@gmail.com>
2017-01-30 10:00:03 -0700

    set up migrations/seeds and initial routes with not all tests passing

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,17 @@
 // Update with your config settings.
-
+'use strict';
 module.exports = {
 
-  development: { },
-  test: { },
-  production: { },
-
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/galvanize_classified_dev'
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/galvanize_classified_test'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  },
 };
--- routes/classifieds.js
+++ routes/classifieds.js
@@ -2,9 +2,60 @@
 'use strict';
 
 const express = require('express');
-
 const router = express.Router();
+const knex = require('../knex');
 
 // YOUR CODE HERE
+
+router.get('/', (req, res, next) => {
+  knex('classifieds')
+    .then((result) => {
+      res.send(result);
+    });
+});
+
+router.get('/:id/', (req, res, next) => {
+  knex('classifieds')
+    .where('id', req.params.id)
+    .then((result) => {
+      res.send(result[0]);
+    });
+});
+
+router.post('/', (req, res, next) => {
+  const { title, description, price, item_image } = req.body;
+  const newPost = { title, description, price, item_image };
+  knex('classifieds')
+    .insert(newPost, '*')
+    .then((result) => {
+      // delete unnecessary returned data
+      res.send(result[0]);
+    });
+});
+
+router.patch('/:id/', (req, res, next) => {
+  const { title, description, price, item_image } = req.body;
+  const newPost = { title, description, price, item_image };
+  knex('classifieds')
+    .where('id', req.params.id)
+    .update(newPost, '*')
+    .then((result) => {
+      res.send(result[0]);
+    });
+});
+
+router.delete('/:id/', (req, res, next) => {
+  knex('classifieds')
+    .where('id', req.params.id)
+    .then((result) => {
+      const post = result[0];
+      knex('classifieds')
+        .where('id', req.params.id)
+        .del()
+        .then(() => {
+          res.send(post);
+        });
+    });
+});
 
 module.exports = router;
